5752264669a10b4961f53eeb5b6c605250575213
dominicpam89 <dominicpam89@gmail.com>
2026-04-30 11:04:28 +0700

    update Layout to have custom tailwind classes

--- src/features/form-experimentation/components/Layout.tsx
+++ src/features/form-experimentation/components/Layout.tsx
@@ -1,10 +1,15 @@
+import { cn } from '@/lib/utils';
 import type { PropsWithChildren } from 'react';
 
-export default function Layout({ children }: PropsWithChildren) {
+interface Props {
+  twClasses?: React.HTMLAttributes<HTMLDivElement>['className'];
+}
+
+export default function Layout({ children, twClasses = '' }: Props & PropsWithChildren) {
   return (
     <div
       aria-label="page-form-experimentation-layout"
-      className="min-h-screen w-full max-w-lg mx-auto pt-12"
+      className={cn('min-h-screen max-w-lg w-full mx-auto pt-12 flex flex-col gap-6', twClasses)}
     >
       {children}
     </div>
