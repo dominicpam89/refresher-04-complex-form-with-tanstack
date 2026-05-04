37681510ff150f113b3299edde165438c596bf4e
dominicpam89 <dominicpam89@gmail.com>
2026-05-04 10:22:14 +0700

    change features/form-experiemntation into features/demo

--- src/lib/router.ts
+++ src/lib/router.ts
@@ -2,7 +2,6 @@ import LayoutCommon from '@/pages/LayoutCommon';
 import PageHome from '@/pages/PageHome';
 import { createBrowserRouter } from 'react-router';
 import PageForm from '@/pages/PageForm';
-import PageFormExperimentation from '@/pages/PageFormExperimentation';
 import PageFormBasic from '@/pages/PageFormBasic';
 
 export const router = createBrowserRouter([
