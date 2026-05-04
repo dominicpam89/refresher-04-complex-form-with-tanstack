772339239bf226605acdd487b2e7b0b5488531ed
dominicpam89 <dominicpam89@gmail.com>
2026-05-04 10:38:56 +0700

    update demo layout

--- src/features/demo/components/Layout.tsx
+++ src/features/demo/components/Layout.tsx
@@ -1,20 +1,44 @@
 import { cn } from '@/lib/utils';
 import type { PropsWithChildren } from 'react';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 
 interface Props {
   twClasses?: React.HTMLAttributes<HTMLDivElement>['className'];
+  cards: {
+    title?: string;
+    description?: string;
+    footer?: React.ReactNode;
+  };
 }
 
-export default function Layout({ children, twClasses = '' }: Props & PropsWithChildren) {
+export default function Layout({
+  children,
+  twClasses = '',
+  cards: { title, description, footer },
+}: Props & PropsWithChildren) {
   return (
     <div
-      aria-label="page-form-experimentation-layout"
+      aria-label="page-form-layout"
       className={cn(
         'min-h-screen max-w-lg min-w-sm mx-auto pt-12 flex flex-col gap-6 p-8 lg:p-0',
         twClasses
       )}
     >
-      {children}
+      <Card className="w-full">
+        <CardHeader>
+          <CardTitle>{title || 'Form'}</CardTitle>
+          <CardDescription>{description || 'Please fill out the form below.'}</CardDescription>
+        </CardHeader>
+        <CardContent>{children}</CardContent>
+        {footer && <CardFooter>{footer}</CardFooter>}
+      </Card>
     </div>
   );
 }
--- src/pages/PageFormBasic.tsx
+++ src/pages/PageFormBasic.tsx
@@ -1,3 +1,15 @@
+import BasicForm from '@/features/demo/components/BasicForm';
+import Layout from '@/features/demo/components/Layout';
+
 export default function PageFormBasic() {
-  return <div>Page Form Basic</div>;
+  return (
+    <Layout
+      cards={{
+        title: 'Basic Form',
+        description: 'This is a basic form with simple validation.',
+      }}
+    >
+      <BasicForm />
+    </Layout>
+  );
 }
