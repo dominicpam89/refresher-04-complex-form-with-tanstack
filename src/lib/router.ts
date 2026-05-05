7ddee3c0f3480225710cbf9b6d722e8ff775f171
dominicpam89 <dominicpam89@gmail.com>
2026-05-05 12:08:24 +0700

    add PageFormSemiComplex.tsx and in router.ts

--- src/lib/router.ts
+++ src/lib/router.ts
@@ -2,6 +2,7 @@ import LayoutCommon from '@/pages/LayoutCommon';
 import PageHome from '@/pages/PageHome';
 import { createBrowserRouter } from 'react-router';
 import PageFormBasic from '@/pages/PageFormBasic';
+import PageFormSemiComplex from '@/pages/PageFormSemiComplex';
 
 export const router = createBrowserRouter([
   { path: '/', Component: PageHome },
@@ -9,5 +10,10 @@ export const router = createBrowserRouter([
     path: '/basic-form',
     Component: LayoutCommon,
     children: [{ index: true, Component: PageFormBasic }],
+  },
+  {
+    path: '/semi-complex-form',
+    Component: LayoutCommon,
+    children: [{ index: true, Component: PageFormSemiComplex }],
   },
 ]);
---
+++ src/pages/PageFormSemiComplex.tsx
@@ -0,0 +1,3 @@
+export default function PageFormSemiComplex() {
+  return <div>PageFormSemi</div>;
+}
