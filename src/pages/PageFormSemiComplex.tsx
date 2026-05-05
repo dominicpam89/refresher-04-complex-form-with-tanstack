ee90e2ee7c03ec3330553310564c7b1bd20c71a4
dominicpam89 <dominicpam89@gmail.com>
2026-05-05 12:26:21 +0700

    establish layout and container for semi-complex-form

--- src/pages/PageFormSemiComplex.tsx
+++ src/pages/PageFormSemiComplex.tsx
@@ -1,3 +1,10 @@
 export default function PageFormSemiComplex() {
-  return <div>PageFormSemi</div>;
+  return (
+    <div
+      aria-label="layout-page-form-semi"
+      className="min-h-screen max-w-lg min-w-sm p-4 lg:p-0 flex justify-center items-center"
+    >
+      Form is here
+    </div>
+  );
 }
