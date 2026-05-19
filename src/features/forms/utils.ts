1301eae7d2f0277de75605736ea1592f628dca6b
dominicpam89 <dominicpam89@gmail.com>
2026-05-19 16:28:27 +0700

    minimum example is established

--- src/features/forms/utils.ts
+++ src/features/forms/utils.ts
@@ -5,20 +5,6 @@ export const formLevels = {
   level4: '4',
   level5: '5',
   level6: '6',
-  level7: '7',
-  level8: '8',
-  level9: '9',
-  level10: '10',
-  level11: '11',
-  level12: '12',
-  level13: '13',
-  level14: '14',
-  level15: '15',
-  level16: '16',
-  level17: '17',
-  level18: '18',
-  level19: '19',
-  level20: '20',
 } as const;
 
 // "level1", "level2"
